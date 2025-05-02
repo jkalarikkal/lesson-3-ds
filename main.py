cbf2e8eaae218e24a1d81322dbbcfdb91442a6f7
jkalarikkal <jkalarikkal007@gmail.com>
2025-05-02 17:54:02 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -22,3 +22,25 @@ print(df["City"])
 print(df["Age"].std())
 print(df["Age"].max())
 
+#started titanic csv file
+
+data = pd.read_csv("titanic.csv")
+
+print(data.head())
+print(data.info())
+
+print(data[["Name","Age"]])
+
+print(data["Age"]>18)
+
+age35 = data[data["Age"]>35]
+
+print(age35.head())
+print(age35.shape)
+
+p2p3 =  data[(data["Pclass"]== 2)| (data["Pclass"] == 3)]
+
+print(p2p3[["Name","Pclass"]].head(20))
+
+
+#Task - Get the mean fare of Male travelling in Pclass 1
